0fd381587b04b9812739254c6b76043681231fd1
twinDrarf <top010301@gmail.com>
2023-07-22 14:57:44 +0900

    ReadyRoom
    1st commit

---
+++ Assets/Scenes/ConnectionManager.cs
@@ -0,0 +1,48 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using TMPro;
+
+public class ConnectionManager : MonoBehaviourPunCallbacks
+{
+    public TextMeshProUGUI ConnectionStatus;
+    public TMP_InputField IDtext;
+    public Button connetBtn;
+
+    void Start()
+    {
+        // Photon PUN 초기화
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void Update()
+    {
+        ConnectionStatus.text = PhotonNetwork.NetworkClientState.ToString();
+    }
+
+    public void Connect()
+    {
+        // 서버에 플레이어 이름 설정
+        PhotonNetwork.LocalPlayer.NickName = IDtext.text;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("서버 접속 완료");
+        
+        // 서버에 연결되면 Connect 버튼을 누를 때 매칭을 시도
+        connetBtn.interactable = true;
+    }
+
+    public void CreateRoom()
+    {
+        // 동일한 룸 이름으로 룸 생성
+        PhotonNetwork.CreateRoom("MyRoom", new RoomOptions { MaxPlayers = 2 });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        print("룸 접속 완료");
+        // 두 명의 플레이어가 매칭되면 이곳에서 게임을 시작하거나 다른 처리를 할 수 있습니다.
+    }
+}

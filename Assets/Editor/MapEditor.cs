f4d0f42816b12cddaad43c7097f3fa5defb62334
JeongSeongwon99 <wjdtjddnjs99@kaist.ac.kr>
2023-07-24 13:02:51 +0900

    slsl

--- Assets/Editor/MapEditor.cs
+++ Assets/Editor/MapEditor.cs
@@ -8,10 +8,14 @@ public class MapEditor : Editor
 {
     public override void OnInspectorGUI()
     {
-        base.OnInspectorGUI();
-
         MapGenerator map = target as MapGenerator;
+        if (DrawDefaultInspector()){
+            map.GenerateMap();
+        }
 
-        map.GenerateMap();
+        if (GUILayout.Button("Generate Map"))
+        {
+            map.GenerateMap();
+        }
     }
 }
